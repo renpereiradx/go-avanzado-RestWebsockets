fa3f700b138f0bf4a4356d81765a43142c33550b
Rene Pereira <renedx92@gmail.com>
2024-03-17 19:12:21 -0300

    Actualización de dependencia gorilla/mux a la versión 1.8.1 en go.mod

---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
+	"github.com/renpereiradx/go-avanzado-RestWebsocket/handler"
+	"github.com/renpereiradx/go-avanzado-RestWebsocket/server"
+)
+
+func main() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	PORT := os.Getenv("PORT")
+	JWT_SECRET := os.Getenv("JWT_SECRET")
+	DATABASE_URL := os.Getenv("DATABASE_URL")
+
+	server, err := server.NewServer(context.Background(), &server.Config{
+		Port:        PORT,
+		JWTSecret:   JWT_SECRET,
+		DatabaseUrl: DATABASE_URL,
+	})
+	if err != nil {
+		log.Fatal("error creating server", err)
+	}
+
+	server.Start(BindRoutes)
+}
+
+func BindRoutes(s server.Server, r *mux.Router) {
+	r.HandleFunc("/", handler.HomeHandler(s)).Methods(http.MethodGet)
+}
