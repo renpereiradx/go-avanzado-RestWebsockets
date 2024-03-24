8e72b7109329f7990fe58072154febdb69717414
Hugo Pereira <renedx92@gmail.com>
2024-03-24 03:55:13 +0000

    Añadir estructura de respuesta y manejador para la ruta de inicio

--- handler/home.go
+++ handler/home.go
@@ -7,11 +7,16 @@ import (
 	"github.com/renpereiradx/go-avanzado-RestWebsocket/server"
 )
 
+// HomeResponse represents the response structure for the home endpoint.
 type HomeResponse struct {
 	Message string `json:"message"`
 	Status  bool   `json:"status"`
 }
 
+// HomeHandler handles the home route and returns a http.HandlerFunc.
+// It takes a server.Server as a parameter and returns a function that handles the request.
+// The function sets the Content-Type header to "application/json",
+// writes a 200 status code to the response writer, and encodes a HomeResponse struct as JSON.
 func HomeHandler(s server.Server) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
---
+++ repository/user.go
@@ -0,0 +1,26 @@
+package repository
+
+import (
+	"context"
+
+	"github.com/renpereiradx/go-avanzado-RestWebsocket/models"
+)
+
+type UserRepository interface {
+	InsertUser(ctx context.Context, user *models.User) error
+	GetUserByID(ctx context.Context, id int64) (*models.User, error)
+}
+
+var implementation UserRepository
+
+func SetUserRepository(user UserRepository) {
+	implementation = user
+}
+
+func InsertUser(ctx context.Context, user *models.User) error {
+	return implementation.InsertUser(ctx, user)
+}
+
+func GetUserByID(ctx context.Context, id int64) (*models.User, error) {
+	return implementation.GetUserByID(ctx, id)
+}
